893f7d32177add42813a884133f17cd1e83733ba
Cheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2024-01-09 14:24:21 -0600

    Created UserData schema
    + User data schema created
    + Updated support server in Config.json

---
+++ src/schemas/UserData.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({
+	clientID: {
+		type: String,
+		required: true
+	},
+	guildID: {
+		type: String,
+		required: true
+	},
+	messages: Number,
+	level: Number,
+	exp: Number,
+	expCap: Number,
+	expMulti: Number,
+	invites: {
+		real: Number,
+		fake: Number,
+		left: Number,
+	}
+});
+
+export default model("userdata", schema);
