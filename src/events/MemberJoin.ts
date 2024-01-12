a025a74f726626fb74320004de849ccfc0c82fd8
Cheese <71863841+WhoCutTheCheese@users.noreply.github.com>
2024-01-12 00:13:13 -0600

    Update MemberJoin.ts

--- src/events/MemberJoin.ts
+++ src/events/MemberJoin.ts
@@ -1,11 +1,32 @@
-import { Events, Guild, GuildMember } from "discord.js";
+import { Collection, Events, Guild, GuildMember, Invite } from "discord.js";
 import { EventsBuilder } from "../structures/EventClass";
 import GuildSettings from "../schemas/GuildSettings";
 import UserData from "../schemas/UserData";
+import { handleError } from "../utilities/HandleError";
+import Invites from "../schemas/Invites";
 
 export default new EventsBuilder()
 	.setEvent(Events.GuildMemberAdd)
 	.setOnce(false)
 	.setExecutor(async (member: GuildMember) => {
+		const userData = new UserData({
+			userID: member.user.id,
+			guildID: member.guild.id
+		});
+		userData.save().catch((err: Error) => handleError(err, "MemberJoin.ts"));
 
+		const invites = await member.guild.invites.fetch();
+		let foundInviter: GuildMember | undefined;
+
+		for (const singleInvite of invites) {
+			const invite = singleInvite[1];
+			const inviteFile = await Invites.findOne({
+				inviteCode: invite.code
+			});
+			if (inviteFile?.uses! < invite.uses!) {
+				foundInviter = await member.guild.members.fetch(inviteFile?.userID!) || undefined;
+				break;
+			}
+		}
+		//if() {}
 	});
